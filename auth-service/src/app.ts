4a8c2c5704953af67972039ab2b10093bea62c57
DungTNT <tuandungbrse@gmail.com>
2021-02-10 21:22:28 +0700

    setting up k8s & ingress-nginx

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -6,4 +6,9 @@ const app = express();
 //
 app.use(json());
 
+// routes
+app.get('/api/users/current-user', (req,res,next) => {
+  res.send('Hi you!');
+})
+
 export default app;
--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -5,4 +5,4 @@ import app from './app';
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8888;
 
-server.listen(PORT, () => console.log(`auth-service is listenning on port ${PORT}`));
+server.listen(PORT, () => console.log(`auth-service is listenning on port ${PORT}!`));
